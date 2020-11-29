e74fd990107aa27938a504e3f2b96f7db01d275b
zycalice <alicezhang727@gmail.com>
2020-11-29 18:48:01 -0500

    added potential path

--- src/BarChartRacerRunner.java
+++ src/BarChartRacerRunner.java
@@ -2,6 +2,7 @@
 public class BarChartRacerRunner {
     public static void main(String[] args) {
         BarChartRacer bcr = new BarChartRacer();
+//        String path = "./data files";
         bcr.readDraw(bcr.getFileName());
     }
 }
