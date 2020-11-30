735c35a931e6b9fd45165d669d85999f038eac39
zycalice <alicezhang727@gmail.com>
2020-11-29 23:07:44 -0500

    edited comments

--- src/BarChartRacerRunner.java
+++ src/BarChartRacerRunner.java
@@ -2,8 +2,11 @@
 public class BarChartRacerRunner {
     public static void main(String[] args) {
         BarChartRacer bcr = new BarChartRacer();
+        bcr.readDraw(bcr.getFileName());
+
+        //you could also uncomment below to set a default file path
 //        String path = "./data files/";
 //        bcr.readDraw(path + bcr.getFileName());
-        bcr.readDraw(bcr.getFileName());
+
     }
 }
